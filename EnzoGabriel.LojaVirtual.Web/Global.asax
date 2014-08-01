a4490ea67f7ef9b976e20aa20fb24b3a54c65087
leandropsouza <leandrop.souza@hotmail.com>
2014-07-31 21:14:33 -0300

    Initial Commit

---
+++ EnzoGabriel.LojaVirtual.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EnzoGabriel.LojaVirtual.Web.MvcApplication" Language="C#" %>
---
+++ EnzoGabriel.Lojavirtual.UnitTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EnzoGabriel.Lojavirtual.UnitTest
+{
+    [TestClass]
+    public class UnitTestEnzoGabriel
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
